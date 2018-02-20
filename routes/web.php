3935fd809f0d6ae1ebbe5683f89a1e14606a80fe
David Cusick <Cusick-D1@ulster.ac.uk>
2018-02-20 21:13:10 +0000

    registration/login routes updated

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,25 @@ Route::resource('feeds', 'FeedController');
 
 Route::get('feed/{slug}', ['as' => 'post.single', 'uses' => 'PostController@getSingle']) -> where('slug', '[\w\d\-\_]+');
 
-Route::get('/login', 'PagesController@getLogin');
+Route::get('post', ['uses' => 'PostController@getIndex', 'as' => 'post.index']);
+
+
+
+//Authentication Routes
+
+Route::get('auth/login', ['as' => 'login', 'uses' =>'Auth\LoginController@Login']);
+
+Route::post('auth/login', 'Auth\LoginController@login');
+
+Route::get('auth/logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+//Route::get('/login', 'PagesController@getLogin');
+
+
+//Registration Routes
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
+
 
 Route::get('/register', 'PagesController@getRegister');
 
