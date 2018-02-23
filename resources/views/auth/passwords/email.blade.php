e55a75498494d0b0dbec11311423c78317630603
David Cusick <Cusick-D1@ulster.ac.uk>
2018-02-23 13:20:19 +0000

    registration/forgot password reset working with email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,37 @@
  @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+ @section('title', 'Reset Password')
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+ @section('content')
+     <div class="container">
+         <div class="row">
+             <div class="col-md-8 col-md-offset-2">
+                 <div class="panel panel-default">
+                     <div class="panel-heading">Reset Password</div>
+                     <div class="panel-body">
+                         @if (session('status'))
+                             <div class="alert alert-success">
+                                 {{ session('status') }}
+                             </div>
+                         @endif
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+                         {!! Form::open(['url' => 'password/email', 'method' => 'POST']) !!}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                         {{ Form::label('email', 'Email Address:') }}
+                         {{ Form::email('email', null, ['class' => 'form-control']) }}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                         <div class="row">
+                             <div class="col-md-4 col-md-offset-4">
+                                 {{ Form::submit('Reset Password', ['class' => 'btn btn-primary btn-block form-spacing-top']) }}
+                             </div>
+                         </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                         {!! Form::close() !!}
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ @endsection
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+
