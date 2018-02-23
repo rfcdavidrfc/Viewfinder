168423c31226b8c40c633c661bbe3c86b694dabe
David Cusick <Cusick-D1@ulster.ac.uk>
2018-02-23 13:20:40 +0000

    registration/forgot password reset working with email

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'davidcusick6@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'David Cusick'),
     ],
 
     /*
