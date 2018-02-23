0c4a14f8b821b8247056dd71de147b01356093c6
David Cusick <Cusick-D1@ulster.ac.uk>
2018-02-23 20:52:48 +0000

    adding categories - will be modifed further to be galleries for posts

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,9 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token?}', 'Auth\ResetPasswordController@showResetForm');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+//Categories
+Route::resource('categories', 'CategoryController', ['except' => ['create']]);
+
 Route::get('/register', 'PagesController@getRegister');
 
 Route::get('/profile', 'PagesController@getProfile');
