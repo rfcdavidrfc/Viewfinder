b89e1a34fae724a23f7bf52ba39270dd14bf44e3
David Cusick <Cusick-D1@ulster.ac.uk>
2018-03-15 20:47:00 +0000

    comments added

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
+use App\Requests;
 use App\Feed;
+use Mail;
 
 class PagesController extends Controller {
 
@@ -22,6 +25,30 @@ class PagesController extends Controller {
 
     public function getContact() {
         return view('pages.contact');
+    }
+
+    public function postContact(Request $request) {
+       $this -> validate($request, [
+           'email'      => 'required|email',
+           'subject'    => 'min:5|max:255',
+           'message'    => 'min:10']);
+
+       $data = array(
+           'email'   => $request -> email,
+           'subject' => $request -> subject,
+           'bodyMessage' => $request -> message,
+           'survey' => ['Q1' => "hello", 'Q2' => 'YES']
+       );
+
+        Mail::send('auth/email/contact', $data, function($message) use ($data){
+
+        $message -> from($data['email']);
+        $message -> to('davidcusick6@gmail.com');
+        $message -> subject($data['subject']);
+
+        });
+
+        return redirect('/feed')->with('success', 'Thank you for contacting us!');
     }
 
     public function getLogin() {
