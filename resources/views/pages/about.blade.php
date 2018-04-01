269dc76b42c7735ddfce18d74dd6975c94f7e4f4
David Cusick <Cusick-D1@ulster.ac.uk>
2018-04-01 22:13:32 +0100

    Fixed my gallery design and added in an about page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', '| About')
+
+@section('content')
+
+    <div class="jumbotron">
+        <h1 class="display-4">About</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="lead">
+ndvjnpvjnspdajvnpodaso vpooksdjv [advkn pasdvn padsjvn pjsvdn vpjsanv apjn adsvpijn dsvpijnvds pjinvdsa pjndsv kjnvds pjn dsvpjn dsvpjn vdspn vdspjn vdsapkjn vdspkjn vdspjn vdsapjn adsvpkjn dspjndsvpkjnsvpkjndsv
+            oisdjv pjnvpjn avpjkn avpkjn vsd pkjn avpjn vpjkn vpjn vsdpkjnvpjn p ibv pijnevwpj nvwpjnvew pibwv pi bwvpij bvwpibvwpj bvw
+            jwhjvb pjw vnpwv jknvwpjn vwpkj wvkjvwp jbwv jbvwkjb vwkj bvwkjb vwkjbvwlkjbvwlkjbwvlkjbvwkjbvwjkbvw jbvwkj bwv ojbwv jbvw bjvw
+            l jbwkjbdvlk jbvwp jbv jbvd bjkdvsj bvdsbj vdsb jvdsbj vd bjvds bjvds bjklvdsbkjl vdsb jlvdsbjk vdsbjk vdsbjk vdsbjk vdskbj vds
+            ksdvb vdsb jvdsbjl vdsjk blvdsbjk vdskljb vsdbjkl vdsbjkl vdsbjk vdsbjk vdsbjl vdsbjkvdsbjk vdsbjkvdskjbdvsbjk vdsbjk
+            j bsdvbj vsd kbjlvdsb jkvdsbjk vdsb jkvdsbjk vdsbjklvdsbjkvdsbjk vdsbjk vdsbjk dvsbjk vdsbjk vdsbjk vdsj vdskbj dvs
+        </p>
+    </div>
+
+@endsection
+
